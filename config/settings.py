9b31b0e2bd89d1dc88a37ecb530e5dea7f185fbd
Brett Berry <brett.berry@getordermark.com>
2022-03-10 19:18:30 -0700

    Adding config

---
+++ config/settings.py
@@ -0,0 +1,27 @@
+class AppConfig:
+    DB: str
+    logging = "test"
+
+    def __init__(self):
+        print("In AppConfig")
+
+
+class DevConfig(AppConfig):
+    DB = "dev"
+
+    def __init__(self):
+        print(" InDevConfig")
+
+
+class StagingConfig(AppConfig):
+    DB = "staging"
+
+    def __init__(self):
+        print("In StagingConfig")
+
+
+class ProdConfig(AppConfig):
+    DB = "prod"
+
+    def __init__(self):
+        print("In ProdConfig")
